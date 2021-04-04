6f6e22c7d1a3bdac8ec1ecab78614fde23008651
Your Name <you@example.com>
2021-04-04 15:01:14 -0300

    vectores

---
+++ vectores.js
@@ -0,0 +1,14 @@
+// distintas maneras de declarar un vector //
+let autos = new Array("auto1","auto2");
+let autos2 = ['auto1'];
+autos3 = [];
+
+//agregar un elemento al final del vector//
+autos.push('Ford');
+
+console.log(autos);
+
+for(i=0;i<autos.length;i++)
+{
+    console.log(autos[i]);
+}
