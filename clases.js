f2c44b420945538a05f4e890470a2bba4207cc2d
Luciano Pulido <lucianopulido1996@gmail.com>
2021-04-09 11:52:18 -0300

    Herencia

--- clases.js
+++ clases.js
@@ -14,6 +14,46 @@ class Persona {
   {
     this.nombre = nombre;
   }
+
+  get obtenerApellido()
+  {
+    return this.apellido;
+  }
+
+  set modificarApellido(apellido)
+  {
+    this.apellido = apellido;
+  }
+
+  nombreCompleto()
+  {
+    return this.nombre+" "+this.apellido;
+  }
+}
+
+// herencia//
+class Empleado extends Persona
+{
+  constructor(nombre,apellido,departamento) 
+  {
+    super(nombre,apellido);// llamo al constructor de la clase padre//
+    this.departamento = departamento;
+  }
+
+  get obtenerDepartamento()
+  {
+    return this.departamento;
+  }
+
+  set modificarDepartamento(departamento)
+  {
+    this.departamento = departamento;
+  }
+
+  nombreCompleto()
+  {
+    return super.nombreCompleto() +" "+this.departamento;
+  }
 }
 
 let persona1 = new Persona("Luciano","Pulido");
@@ -21,4 +61,7 @@ console.log(persona1);
 console.log(persona1.obtenerNombre);
 persona1.nombre = "Marcelo";
 console.log(persona1.obtenerNombre);
+
+let empleado = new Empleado("Luciano","Pulido","Ingenieria");
+console.log("Empleado: "+empleado.nombreCompleto());
 
